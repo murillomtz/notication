832273c19c596b02cabf65341831c070d5dcd794
murillomtz <60979842+murillomtz@users.noreply.github.com>
2024-04-09 12:47:04 -0300

    first commit

---
+++ src/main/java/com/gestaohospitalarpessoas/notication/NoticationApplication.java
@@ -0,0 +1,13 @@
+package com.gestaohospitalarpessoas.notication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestaohospitalarpessoas/notication/NoticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaohospitalarpessoas.notication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
